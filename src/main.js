fb396f5a5791882d234004a49a7344f60f072742
lquinto13 <lancegabrielquinto@gmail.com>
2023-07-26 02:06:50 +0800

    Fix css import

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import '../styles/modern-normalize.css'
-import '../styles/style.css'
-import '../styles/components/login.css'
-import '../styles/components/header.css'
-import '../styles/components/nav.css'
-import '../styles/components/main.css'
-import '../styles/utils.css'
+import '../../styles/modern-normalize.css'
+import '../../styles/style.css'
+import '../../styles/components/login.css'
+import '../../styles/components/header.css'
+import '../../styles/components/nav.css'
+import '../../styles/utils.css'
 
 
