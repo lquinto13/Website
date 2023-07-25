9d1cebd43c75a487d68fe4d6e8c7e079ff95bc2f
lquinto13 <lancegabrielquinto@gmail.com>
2023-07-26 02:00:23 +0800

    Push website code to github

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import '../styles/modern-normalize.css'
+import '../styles/style.css'
+import '../styles/components/login.css'
+import '../styles/components/header.css'
+import '../styles/components/nav.css'
+import '../styles/components/main.css'
+import '../styles/utils.css'
+
+
